a2288e6df6b9be3ddbcc9d32c6b9a028da76f1f4
liuzhuang <70458055@qq.com>
2019-08-15 18:34:55 +0800

    :construction: init enterprise

---
+++ src/views/report/enterprise/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="report-enterprise-wrapper">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReportEnterprise'
+}
+</script>
+
+<style lang="less" scoped>
+  .report-enterprise-wrapper {
+    position: relative;
+    width: 1920px;
+    height: 1080px;
+    overflow: hidden;
+  }
+</style>
