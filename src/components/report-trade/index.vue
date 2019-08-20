db1da2aa88675568e4377c234b241c4b1cbb1c9e
lz82 <nickylau82@gmail.com>
2019-08-20 23:44:24 +0800

    :construction: init report trade page

---
+++ src/components/report-trade/index.vue
@@ -0,0 +1,290 @@
+<template>
+  <div class="report-trade-wrapper">
+    <div ref="mychart" style="width: 100%; height: 910px"></div>
+  </div>
+</template>
+
+<script>
+import reportConfig from '@/config/report'
+const { colorList } = reportConfig
+
+const rich = {
+  num: {
+    fontWeight: 700,
+    fontSize: 24,
+    color: '#fff',
+    align: 'center'
+  },
+  title: {
+    fontSize: 20,
+    color: '#fff',
+    align: 'center'
+  }
+}
+export default {
+  name: 'ReportTrade',
+
+  data () {
+    return {
+      chart: null,
+      option: {
+        animationDurationUpdate: 1500,
+        animationEasingUpdate: 'quinticInOut',
+        color: reportConfig.colorList,
+        series: [
+          {
+            type: 'graph',
+            layout: 'force',
+            force: {
+              initLayout: 'circular',
+              repulsion: 1000,
+              edgeLength: 250,
+              focusNodeAdjacency: true
+            },
+            roam: false,
+            label: {
+              normal: {
+                show: true
+              }
+            },
+            data: [
+              {
+                name: 'AI投资总额',
+                symbolSize: 220,
+                draggable: true,
+                category: 0,
+                itemStyle: {
+                  normal: {
+                    borderColor: colorList[0],
+                    borderWidth: 6,
+                    shadowBlur: 20,
+                    shadowColor: colorList[0],
+                    color: '#001c43'
+                  }
+                },
+                label: {
+                  show: true,
+                  formatter () {
+                    return '{num|1000万}\n{title|AI投资总额}'
+                  },
+                  rich: rich
+                }
+              },
+              {
+                name: '汽车交通',
+                symbolSize: 200,
+                draggable: true,
+                itemStyle: {
+                  normal: {
+                    borderColor: colorList[1],
+                    borderWidth: 4,
+                    shadowBlur: 10,
+                    shadowColor: colorList[1],
+                    color: '#001c43'
+                  }
+                },
+                label: {
+                  show: true,
+                  formatter () {
+                    return '{num|200万}\n{title|汽车交通}'
+                  },
+                  rich: rich
+                }
+              },
+              {
+                name: '游戏',
+                symbolSize: 200,
+                category: 1,
+                itemStyle: {
+                  normal: {
+                    borderColor: colorList[2],
+                    borderWidth: 4,
+                    shadowBlur: 10,
+                    shadowColor: colorList[2],
+                    color: '#001c43'
+                  }
+                },
+                label: {
+                  show: true,
+                  formatter () {
+                    return '{num|100万}\n{title|游戏}'
+                  },
+                  rich: rich
+                }
+              },
+              {
+                name: '电子商务',
+                symbolSize: 210,
+                category: 1,
+                itemStyle: {
+                  normal: {
+                    borderColor: colorList[3],
+                    borderWidth: 4,
+                    shadowBlur: 10,
+                    shadowColor: colorList[3],
+                    color: '#001c43'
+                  }
+                },
+                label: {
+                  show: true,
+                  formatter () {
+                    return '{num|140万}\n{title|电子商务}'
+                  },
+                  rich: rich
+                }
+              },
+              {
+                name: '硬件',
+                symbolSize: 160,
+                category: 1,
+                itemStyle: {
+                  normal: {
+                    borderColor: colorList[4],
+                    borderWidth: 4,
+                    shadowBlur: 10,
+                    shadowColor: colorList[4],
+                    color: '#001c43'
+                  }
+                },
+                label: {
+                  show: true,
+                  formatter () {
+                    return '{num|100万}\n{title|硬件}'
+                  },
+                  rich: rich
+                }
+              },
+              {
+                name: '社交网络',
+                symbolSize: 200,
+                category: 2,
+                itemStyle: {
+                  normal: {
+                    borderColor: colorList[5],
+                    borderWidth: 4,
+                    shadowBlur: 10,
+                    shadowColor: colorList[5],
+                    color: '#001c43'
+                  }
+                },
+                label: {
+                  show: true,
+                  formatter () {
+                    return '{num|100万}\n{title|社交网络}'
+                  },
+                  rich: rich
+                }
+              }
+              // {
+              //   name: '医疗健康',
+              //   symbolSize: 80,
+              //   category: 2,
+              //   itemStyle: {
+              //     normal: {
+              //       borderColor: '#b457ff',
+              //       borderWidth: 4,
+              //       shadowBlur: 10,
+              //       shadowColor: '#b457ff',
+              //       color: '#001c43'
+              //     }
+              //   }
+
+              // },
+              // {
+              //   name: '教育',
+              //   symbolSize: 80,
+              //   itemStyle: {
+              //     normal: {
+              //       borderColor: '#82dffe',
+              //       borderWidth: 4,
+              //       shadowBlur: 10,
+              //       shadowColor: '#04f2a7',
+              //       color: '#001c43'
+
+              //     }
+              //   },
+              //   category: 2
+
+              // },
+              // {
+              //   name: '物流',
+              //   symbolSize: 80,
+              //   itemStyle: {
+              //     normal: {
+              //       borderColor: '#82dffe',
+              //       borderWidth: 4,
+              //       shadowBlur: 10,
+              //       shadowColor: '#04f2a7',
+              //       color: '#001c43'
+              //     }
+              //   },
+              //   category: 2
+
+              // }
+            ],
+            links: [
+              {
+                source: 'AI投资总额',
+                target: '汽车交通'
+
+              },
+              {
+                source: 'AI投资总额',
+                target: '游戏'
+              },
+              {
+                source: 'AI投资总额',
+                target: '电子商务'
+              },
+              {
+                source: 'AI投资总额',
+                target: '硬件'
+              },
+              {
+                source: 'AI投资总额',
+                target: '社交网络'
+              },
+              {
+                source: 'AI投资总额',
+                target: '医疗健康'
+              },
+              {
+                source: 'AI投资总额',
+                target: '教育'
+              },
+              {
+                source: 'AI投资总额',
+                target: '物流'
+              }
+            ],
+            lineStyle: {
+              normal: {
+                opacity: 0.9,
+                width: 5,
+                curveness: 0
+              }
+            }
+          }
+        ]
+      }
+    }
+  },
+
+  mounted () {
+    this.$nextTick(() => {
+      this.initChart()
+    })
+  },
+
+  methods: {
+    initChart () {
+      this.chart = this.$echarts.init(this.$refs.mychart)
+      this.chart.setOption(this.option)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/views/report/invest/index.vue
+++ src/views/report/invest/index.vue
@@ -1,12 +1,70 @@
 <template>
   <div class="report-invest-wrapper">
-    this is invest page
+    <div class="left-wrapper">
+      <header>
+        <page-header
+          title="人工智能投资分布"
+        />
+      </header>
+      <div class="left-content">
+        <report-trade />
+      </div>
+    </div>
+    <div class="right-wrapper">
+      <div class="btn-wrapper">
+        <btn-group />
+      </div>
+      <div class="content">
+        <report-card
+          title="近10年各领域投资额增长"
+          width="100%"
+          height="310px"
+        >
+        </report-card>
+
+        <div class="double-wrapper">
+          <report-card
+            class="half"
+            title="专利增长"
+            width="320px"
+            height="260px"
+          >
+          </report-card>
+
+          <report-card
+            class="half"
+            title="Top5 投资领域"
+            width="320px"
+            height="260px"
+          >
+          </report-card>
+        </div>
+        <report-card
+          title="投资机构热度排行"
+          width="100%"
+          height="260px"
+        >
+        </report-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import PageHeader from '@/components/page-header'
+import BtnGroup from '@/components/btn-group'
+import ReportCard from '@/components/report-card'
+import ReportTrade from '@/components/report-trade'
+
 export default {
   name: 'ReportInvest',
+
+  components: {
+    PageHeader,
+    BtnGroup,
+    ReportCard,
+    ReportTrade
+  },
 
   data () {
     return {}
@@ -18,6 +76,7 @@ export default {
   .report-invest-wrapper {
     display: flex;
     flex-flow: row nowrap;
+    justify-content: space-around;
     width: 1920px;
     height: 1080px;
     background: url('../../home/img/bg.png') center no-repeat fixed;
@@ -25,5 +84,41 @@ export default {
     color: #fff;
     padding: 40px;
     box-sizing: border-box;
+
+    .left-wrapper {
+      flex: 0 0 1130px;
+      display: flex;
+      flex-flow: column nowrap;
+
+      header {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: center;
+      }
+    }
+
+    .right-wrapper {
+      flex: 0 0 670px;
+      display: flex;
+      flex-flow: column nowrap;
+      .btn-wrapper {
+        padding-top: 10px;
+        padding-bottom: 30px;
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: flex-end;
+      }
+
+      .content {
+        .double-wrapper {
+          display: flex;
+          flex-flow: row nowrap;
+          justify-content: space-between;
+          .half {
+            flex: 0 0 48%;
+          }
+        }
+      }
+    }
   }
 </style>
