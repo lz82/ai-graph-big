e9591b8f39cacf4378831c3a655fd4f918c83406
liuzhuang <70458055@qq.com>
2019-08-21 16:00:35 +0800

    :construction: complete report invest page

---
+++ src/components/report-img-bar/index.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="report-img-bar-wrapper">
+    <div ref="mychart" style="width: 100%; height: 170px"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReportImgBar',
+
+  data () {
+    return {
+      chart: null,
+      option: {
+        grid: {
+          left: '10px',
+          top: '20px',
+          right: '50px',
+          bottom: '10px',
+          containLabel: true
+        },
+        xAxis: {
+          splitLine: {
+            show: false
+          },
+          axisLine: {
+            show: false
+          },
+          axisLabel: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          }
+        },
+        yAxis: [
+          {
+            type: 'category',
+            inverse: false,
+            data: ['领域A', '领域B', '领域C', '领域D', '领域E'],
+            axisLine: {
+              show: false
+            },
+            axisTick: {
+              show: false
+            },
+            splitLine: {
+              show: false,
+              lineStyle: {
+                type: 'dashed',
+                color: '#3e86dd'
+              }
+            },
+            axisLabel: {
+              margin: 10,
+              textStyle: {
+                color: '#fff',
+                fontSize: 14
+              }
+            }
+          }
+        ],
+        series: [
+          {
+            tooltip: {
+              show: false
+            },
+            z: 4,
+            type: 'pictorialBar',
+            symbolSize: ['20', '20'],
+            symbolRepeat: 'fixed',
+            data: [
+              {
+                value: 1,
+                symbol: 'image://' + '/img/b.png'
+              },
+              {
+                value: 1,
+                symbol: 'image://' + '/img/b.png'
+              },
+              {
+                value: 1,
+                symbol: 'image://' + '/img/b.png'
+              },
+              {
+                value: 1,
+                symbol: 'image://' + '/img/b.png'
+              },
+              {
+                value: 1,
+                symbol: 'image://' + '/img/b.png'
+              }
+            ]
+          },
+          {
+            z: 6,
+            type: 'pictorialBar',
+            symbolSize: ['20', '20'],
+            animation: true,
+            symbolRepeat: 'fixed',
+            symbolClip: true,
+            symbolPosition: 'start',
+            symbolOffset: [0, 0],
+            data: [
+              {
+                value: 60,
+                symbol: 'image://' + '/img/a.png'
+              },
+              {
+                value: 85,
+                symbol: 'image://' + '/img/a.png'
+              },
+              {
+                value: 120,
+                symbol: 'image://' + '/img/a.png'
+              },
+              {
+                value: 140,
+                symbol: 'image://' + '/img/a.png'
+              },
+              {
+                value: 160,
+                symbol: 'image://' + '/img/a.png'
+              }
+            ],
+            label: {
+              normal: {
+                show: true,
+                textStyle: {
+                  color: '#FFF',
+                  fontSize: 14
+                },
+                position: 'right',
+                offset: [15, 0]
+              }
+            }
+          },
+          {
+            type: 'bar'
+          }
+        ]
+      }
+    }
+  },
+
+  mounted () {
+    this.$nextTick(() => {
+      this.initChart()
+    })
+  },
+
+  methods: {
+    initChart () {
+      this.chart = this.$echarts.init(this.$refs.mychart)
+      this.chart.setOption(this.option)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/views/report/invest/index.vue
+++ src/views/report/invest/index.vue
@@ -20,15 +20,25 @@
           width="100%"
           height="310px"
         >
+          <report-muliti-line
+            width="100%"
+            height="220px"
+            :legend="domainList"
+            :xaxis="['2014', '2015', '2016', '2017', '2018']"
+            :series="rateData"
+          />
         </report-card>
 
         <div class="double-wrapper">
           <report-card
             class="half"
-            title="专利增长"
+            title="投资阶段分布"
             width="320px"
             height="260px"
           >
+            <report-light-pie
+              :series="petentRate"
+            />
           </report-card>
 
           <report-card
@@ -37,6 +47,7 @@
             width="320px"
             height="260px"
           >
+            <report-img-bar />
           </report-card>
         </div>
         <report-card
@@ -44,6 +55,24 @@
           width="100%"
           height="260px"
         >
+          <div class="table">
+              <div class="row header">
+                <span>机构名称</span>
+                <span>主投领域</span>
+                <span>基金</span>
+                <span>被投明星企业</span>
+              </div>
+              <template
+                v-for="(item, index) in investList"
+              >
+                <div class="row" :key="index">
+                  <span>{{item.name}}</span>
+                  <span>{{item.domain}}</span>
+                  <span>{{item.fund}}</span>
+                  <span>{{item.company}}</span>
+                </div>
+              </template>
+            </div>
         </report-card>
       </div>
     </div>
@@ -55,6 +84,9 @@ import PageHeader from '@/components/page-header'
 import BtnGroup from '@/components/btn-group'
 import ReportCard from '@/components/report-card'
 import ReportTrade from '@/components/report-trade'
+import ReportMulitiLine from '@/components/report-muliti-line'
+import ReportLightPie from '@/components/report-light-pie'
+import ReportImgBar from '@/components/report-img-bar'
 
 export default {
   name: 'ReportInvest',
@@ -63,11 +95,94 @@ export default {
     PageHeader,
     BtnGroup,
     ReportCard,
-    ReportTrade
+    ReportTrade,
+    ReportMulitiLine,
+    ReportLightPie,
+    ReportImgBar
   },
 
   data () {
-    return {}
+    return {
+      domainList: ['机器学习', '人机混合', '知识系统', '神经网络', '人脸识别', '声音识别'],
+      rateData: [
+        {
+          name: '机器学习',
+          data: [1000, 1200, 1400, 1600, 1800]
+        },
+        {
+          name: '人机混合',
+          data: [2000, 2300, 1400, 1600, 1800]
+        },
+        {
+          name: '知识系统',
+          data: [3000, 2400, 1400, 1600, 1800]
+        },
+        {
+          name: '神经网络',
+          data: [4000, 2500, 1400, 1600, 1800]
+        },
+        {
+          name: '人脸识别',
+          data: [5000, 1600, 1400, 1600, 1800]
+        },
+        {
+          name: '声音识别',
+          data: [6000, 1700, 1400, 1600, 1800]
+        }
+      ],
+      petentRate: [
+        {
+          name: '天使轮',
+          value: 100
+        },
+        {
+          name: 'A轮',
+          value: 80
+        },
+        {
+          name: 'A+轮',
+          value: 70
+        },
+        {
+          name: 'B轮',
+          value: 60
+        },
+        {
+          name: 'C轮',
+          value: 50
+        },
+        {
+          name: 'IPO',
+          value: 10
+        }
+      ],
+      investList: [
+        {
+          name: '红杉资本',
+          domain: '机器学习',
+          fund: '上投摩根基金公司',
+          company: '上海科技发展有限公司'
+        },
+        {
+          name: '红杉资本',
+          domain: '机器学习',
+          fund: '上投摩根基金公司',
+          company: '上海科技发展有限公司'
+        },
+        {
+          name: '红杉资本',
+          domain: '机器学习',
+          fund: '上投摩根基金公司',
+          company: '上海科技发展有限公司'
+        },
+        {
+          name: '红杉资本',
+          domain: '机器学习',
+          fund: '上投摩根基金公司',
+          company: '上海科技发展有限公司'
+        }
+      ]
+    }
   }
 }
 </script>
@@ -117,6 +232,28 @@ export default {
           .half {
             flex: 0 0 48%;
           }
+        }
+      }
+
+      .table {
+        width: 100%;
+        display: flex;
+        flex-flow: column nowrap;
+        .row {
+          display: flex;
+          flex-flow: row nowrap;
+          justify-content: space-between;
+          align-items: center;
+          line-height: 30px;
+          font-size: 12px;
+          span {
+            flex: 0 0 25%;
+            text-align: center;
+          }
+        }
+        .header {
+          border-bottom: solid 3px #495377;
+          line-height: 40px;
         }
       }
     }
