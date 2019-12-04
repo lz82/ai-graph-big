ed733caf136a07087fd7279072c0a8ad299c4324
liuzhuang <70458055@qq.com>
2019-12-04 15:25:47 +0800

    :brench: modify eslint config

--- src/service/index.js
+++ src/service/index.js
@@ -1,11 +1,7 @@
 import * as searchApi from './search'
-import * as detailApi from './detail'
 import * as graphApi from './graph'
-import * as reportApi from './report'
 
 export {
   searchApi,
-  detailApi,
-  graphApi,
-  reportApi
+  graphApi
 }
